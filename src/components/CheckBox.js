657742d5bf1a1d74c941907f53d06c7b50b5d492
Casey Metz <caseymetz@gmail.com>
2016-11-01 14:58:00 -0600

    experiment with checkbox binding

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -5,6 +5,7 @@ module.exports = (props) => {
     <label><input type="checkbox"
             className={props.source}
             onChange={() => { props.pickSource(props.source);} }
+            defaultChecked={props.bool}
             />
       {props.name}
     </label>
