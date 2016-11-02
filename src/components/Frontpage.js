1ea89c6bb9912a3e9389be18beafb388e10b5dc2
Casey Metz <caseymetz@gmail.com>
2016-11-01 19:50:35 -0600

    add link from settings to home

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,43 +1,34 @@
 import React from "react";
 import Article from "./Article";
-import _ from "lodash";
 
-const Frontpage = ( props ) => {
+class Frontpage extends React.Component{
 
-  const articles1 = props.frontPageArticles.map((source)=>source.articles);
-  console.log(articles1);
-  const allArticles = _.flatten(articles1);
+  constructor(){
+    super();
+  }
 
-  return (
-    <div className="main-wrapper">
-      <section>
-        <button
-          onClick={ () => { props.frontPageSources.forEach((source) => {
-                            props.fetchFrontPageArticles(source.id); });
-                          }
-                        }>
-          Get content
-        </button>
+  componentWillMount(){
+    this.props.frontPageSources.forEach((source) => {
+      this.props.fetchFrontPageArticles(source.id); });
+  }
+
+  render(){
+
+    const articles1 = this.props.frontPageArticles.map((source)=>source.articles);
+
+    return (
+      <div className="main-wrapper">
         <section>
-          { props.frontPageArticles.length > 0 ? articles1.map((item)=>item.map((article)=><Article a={article}/>))
-                                                  : <p>show stuff here</p> }
+            { articles1.length > 0 ?
+              articles1.map(item =>
+                item.map(article =>
+                  <Article a={article}/>
+                )
+              ) : <p>No sources selected</p> }
         </section>
-      </section>
-    </div>
-  );
-
-};
+      </div>
+    );
+  }
+}
 
 export default Frontpage;
-
-
-
-// { props.frontPageSources.articles[0] ?
-//   props.frontPageSources.articles.map( (article) => {
-//     return (<ul>
-//               <h3>{article.title}</h3>
-//               <p>{article.author}</p>
-//               <p>{article.description}</p>
-//               <a href={article.url}>Link to full article</a>
-//             </ul>);
-//           })   : 'Press button to get content' }
