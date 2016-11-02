1f65d897a889287c0e1239d7591bfe766b8089d9
Casey Metz <caseymetz@gmail.com>
2016-11-01 19:01:01 -0600

    dynamically render checkboxes!

--- src/components/SelectionForm.js
+++ src/components/SelectionForm.js
@@ -7,16 +7,14 @@ module.exports = (props) => {
 
   return (
     <div className="selection-container">
-      <CheckBox source={'the-new-york-times'} name={'The New York Times'} /><br/>
-      <CheckBox source={'the-washington-post'} name={'The Washington Post'} /><br/>
-      <CheckBox source={'the-economist'} name={'The Economist'} /><br/>
-      <CheckBox source={'cnn'} name={'CNN'} /><br/>
-      <CheckBox source={'bbc-news'} name={'BBC News'} /><br/>
-      <CheckBox source={'associated-press'} name={'Associated Press'} /><br/>
-      <CheckBox source={'reuters'} name={'Reuters'} /><br/>
-      <CheckBox source={'the-huffington-post'} name={'The Huffington Post'} /><br/>
-      <CheckBox source={'usa-today'} name={'USA Today'} /><br/>
-      <CheckBox source={'the-wall-street-journal'} name={'The Wall Street Journal'} /><br/>
+      {props.allSources.main.map(item => <div>
+                                            <CheckBox
+                                              source={item.id}
+                                              name={item.name}
+                                              bool={item.picked}
+                                            />
+                                            <br/>
+                                          </div>)}
       <button
         onClick={ () => { props.setFrontSources();
                           props.frontPageSources.forEach((source) => {
@@ -29,3 +27,19 @@ module.exports = (props) => {
   );
 
 };
+
+
+
+
+
+
+// <CheckBox source={'the-new-york-times'} name={'The New York Times'} /><br/>
+// <CheckBox source={'the-washington-post'} name={'The Washington Post'} /><br/>
+// <CheckBox source={'the-economist'} name={'The Economist'} /><br/>
+// <CheckBox source={'cnn'} name={'CNN'} /><br/>
+// <CheckBox source={'bbc-news'} name={'BBC News'} /><br/>
+// <CheckBox source={'associated-press'} name={'Associated Press'} /><br/>
+// <CheckBox source={'reuters'} name={'Reuters'} /><br/>
+// <CheckBox source={'the-huffington-post'} name={'The Huffington Post'} /><br/>
+// <CheckBox source={'usa-today'} name={'USA Today'} /><br/>
+// <CheckBox source={'the-wall-street-journal'} name={'The Wall Street Journal'} /><br/>
