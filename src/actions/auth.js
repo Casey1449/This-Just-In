18f90e261acc3c6feb299affe70b8b5b5821c613
Casey Metz <caseymetz@gmail.com>
2016-11-06 11:58:03 -0700

    add firebase config and init

---
+++ src/actions/auth.js
@@ -0,0 +1,11 @@
+var firebase = require("firebase");
+
+var config = {
+  apiKey: "AIzaSyDfp5U4FX_xgZVls-neLDarIIsq8l5pJEw",
+  authDomain: "this-just-in-74b5b.firebaseapp.com",
+  databaseURL: "https://this-just-in-74b5b.firebaseio.com",
+  storageBucket: "this-just-in-74b5b.appspot.com",
+  messagingSenderId: "393777810926"
+};
+
+firebase.initializeApp(config);
