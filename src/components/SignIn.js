8c3286a42aca1944027f9f61dc1734e4bef236ce
Casey Metz <caseymetz@gmail.com>
2016-11-09 22:57:00 -0700

    fix a style thing

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return (
     <section>
-      <h1 className='main-banner'>Please log in above to view your paper</h1>
+      <h1>Please log in above to view your paper</h1>
     </section>
   );
 };
